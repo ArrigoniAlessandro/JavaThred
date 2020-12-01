9a1e693a2710c260f2e5eab2995b7626946d10a6
arrigoni.alessandro <arrigoni.alessandro@TAVE-PC09.OSDBSESTO.local>
2020-12-01 13:42:16 +0100

    Codice in corso

---
+++ src/javathread/Conto.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javathread;
+
+/**
+ *
+ * @author arrigoni.alessandro
+ */
+public class Conto {
+ private int totale; 
+ private int tasse = 100;
+
+ public Conto(int totale){
+ this.totale = totale;     
+ }
+ 
+synchronized void versa (int somma){
+ totale = totale + somma - tasse;    
+}
+
+synchronized int saldo (){
+    return totale;  
+} 
+
+}
